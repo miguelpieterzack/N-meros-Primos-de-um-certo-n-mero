7249dc8e0e0552b861be3ec520e5d38bd5ed1bab
Miguel Pieterzack <25015@fatectq.local>
2026-06-15 08:41:40 -0300

    Primeiro commit

---
+++ Cadastro.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Estruturas
+{
+    partial class frmCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastro));
+            pibImagem = new PictureBox();
+            btnFechar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pibImagem).BeginInit();
+            SuspendLayout();
+            // 
+            // pibImagem
+            // 
+            pibImagem.Image = (Image)resources.GetObject("pibImagem.Image");
+            pibImagem.Location = new Point(47, 30);
+            pibImagem.Name = "pibImagem";
+            pibImagem.Size = new Size(213, 237);
+            pibImagem.TabIndex = 0;
+            pibImagem.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            btnFechar.Location = new Point(98, 285);
+            btnFechar.Name = "btnFechar";
+            btnFechar.Size = new Size(106, 40);
+            btnFechar.TabIndex = 1;
+            btnFechar.Text = "Sair";
+            btnFechar.UseVisualStyleBackColor = true;
+            btnFechar.Click += btnFechar_Click;
+            // 
+            // frmCadastro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnFechar);
+            Controls.Add(pibImagem);
+            Name = "frmCadastro";
+            Text = "Cadastro";
+            ((System.ComponentModel.ISupportInitialize)pibImagem).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pibImagem;
+        private Button btnFechar;
+    }
+}
---
+++ Principal.cs
@@ -0,0 +1,164 @@
+namespace Estruturas
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            string usuario = "admin";
+            string senha = "Tente123@";
+
+            if (usuario == txtUsuario.Text & senha == txtSenha.Text)
+            {
+                frmCadastro cadastro = new frmCadastro();
+                cadastro.FormBorderStyle = FormBorderStyle.None;
+                cadastro.Bounds = Screen.PrimaryScreen.Bounds;
+                cadastro.TopMost = true;
+                cadastro.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorretos!!", "Verificação",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question
+                );
+                txtUsuario.Focus();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtUsuario.Clear();
+            txtSenha.Clear();
+            txtUsuario.Focus();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            int cont = 5;
+            lsbMostra.Items.Clear();
+            double fatorial = 1;
+
+            while (cont <= 5 & cont >= 1)
+            {
+                fatorial *= cont;
+                cont--;
+                lsbMostra.Items.Add(fatorial.ToString());
+            }
+
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            int num = 1;
+            lsbMostra.Items.Clear();
+
+            do
+            {
+                lsbMostra.Items.Add((num * num).ToString());
+                num++;
+            }
+            while (num <= 5);
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            int maisvc = 0;
+            lsbMostra.Items.Clear();
+
+            for (maisvc = 1; maisvc <= 25000; maisvc *= 5)
+            {
+                lsbMostra.Items.Add(maisvc.ToString());
+            }
+        }
+
+        private void btnForEach_Click(object sender, EventArgs e)
+        {
+            // Array ou vetor é uma estrutura de dados que armazena
+            // valores que podem ser acessados por uma posição
+            // frutas[0] = "Maça", frutas[1] = "Mamão",.....
+            string[] frutas = ["Maça", "Mamão", "Abacaxi", "Limão"];
+
+            lsbMostra.Items.Clear();
+            // lsbMostra.Items.Add(frutas[0]);
+            // lsbMostra.Items.Add(frutas[1]);
+
+            foreach (string coisas in frutas)
+            {
+                lsbMostra.Items.Add(coisas);
+            }
+        }
+
+        private void btnBreak_Click(object sender, EventArgs e)
+        {
+            int cont = 5;
+            lsbMostra.Items.Clear();
+            double fatorial = 1;
+
+            while (cont <= 5 & cont >= 1)
+            {
+                fatorial *= cont;
+                cont--;
+                if (cont == 2)
+                {
+                    break;
+                }
+                lsbMostra.Items.Add(fatorial.ToString());
+            }
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            int cont = 5;
+            lsbMostra.Items.Clear();
+            double fatorial = 1;
+
+            while (cont <= 5 & cont >= 1)
+            {
+                fatorial *= cont;
+                cont--;
+
+                if (cont == 2)
+                {
+                    continue;
+                }
+                lsbMostra.Items.Add(fatorial.ToString());
+            }
+        }
+
+        private void btnPrimos_Click(object sender, EventArgs e)
+        {
+            lsbMostra.Items.Clear();
+
+            for (int candidato = 2; candidato <= 67; candidato++)
+            {
+                bool ePrimo = true;
+                int divisor = 2;
+
+                while (divisor * divisor <= candidato)
+                {
+                    if (candidato % divisor == 0)
+                    {
+                        ePrimo = false;
+                        break;
+                    }
+                    divisor++;
+                }
+
+                if (ePrimo)
+                {
+                    lsbMostra.Items.Add(candidato.ToString());
+                }
+            }
+        }
+    }
+}
